81a91adc92218764acca6f2507ca062504c9a614
NicholasBoss <NicholasBoss0914@gmail.com>
2023-12-28 10:32:53 -0700

    Fixed timing of redirect.js

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -14,29 +14,29 @@ let countdown = setInterval(function() {
     if (count === 0 && username =='admin' && password == 'admin') {
         clearInterval(countdown);
         msg.textContent = "Redirecting to admin page...";
-        // pause for 2 seconds before redirecting
-        let count = 2;
+        // pause for 1 second before redirecting
+        let count = 1;
         document.querySelector('.count-down').textContent = count;
         setTimeout(function() {
             window.location.href = "conceptworkshopadmin.html";
-        }, 2000);
+        }, 1000);
     } else if (count === 0 && username =='student' && password == 'student') {
         clearInterval(countdown);
         msg.textContent = "Redirecting to student page...";
-        let count = 2;
+        let count = 1;
         document.querySelector('.count-down').textContent = count;
         setTimeout(function() {
             window.location.href = "conceptworkshopstudent.html";
-        }, 2000);
+        }, 1000);
     } // if user used wrong password, redirect to home page with message
     else if (count === 0) {
         clearInterval(countdown);
         msg.textContent = "Wrong username or password. Redirecting to home page...";
-        // pause for 2 seconds before redirecting
-        let count = 2;
+        // pause for 1 second before redirecting
+        let count = 1;
         document.querySelector('.count-down').textContent = count;
         setTimeout(function() {
             window.location.href = "index.html";
-        }, 2000);
+        }, 1000);
     }
 }, 1000);
