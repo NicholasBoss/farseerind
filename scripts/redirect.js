ad1a1e875240915e4ba0a0af37df924a75b8a439
NicholasBoss <NicholasBoss0914@gmail.com>
2023-12-28 10:29:20 -0700

    fixed login form, redirect page, and redirect.js to handle invalid login attempts.

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -2,6 +2,7 @@
 let url = new URL(window.location)
 let params = url.searchParams
 
+let msg = document.querySelector('.login-msg')
 let username = params.get('uname')
 let password = params.get('psw')
 
@@ -12,9 +13,30 @@ let countdown = setInterval(function() {
     document.querySelector('.count-down').textContent = count;
     if (count === 0 && username =='admin' && password == 'admin') {
         clearInterval(countdown);
-        window.location.href = "conceptworkshopadmin.html";
+        msg.textContent = "Redirecting to admin page...";
+        // pause for 2 seconds before redirecting
+        let count = 2;
+        document.querySelector('.count-down').textContent = count;
+        setTimeout(function() {
+            window.location.href = "conceptworkshopadmin.html";
+        }, 2000);
     } else if (count === 0 && username =='student' && password == 'student') {
         clearInterval(countdown);
-        window.location.href = "conceptworkshopstudent.html";
+        msg.textContent = "Redirecting to student page...";
+        let count = 2;
+        document.querySelector('.count-down').textContent = count;
+        setTimeout(function() {
+            window.location.href = "conceptworkshopstudent.html";
+        }, 2000);
+    } // if user used wrong password, redirect to home page with message
+    else if (count === 0) {
+        clearInterval(countdown);
+        msg.textContent = "Wrong username or password. Redirecting to home page...";
+        // pause for 2 seconds before redirecting
+        let count = 2;
+        document.querySelector('.count-down').textContent = count;
+        setTimeout(function() {
+            window.location.href = "index.html";
+        }, 2000);
     }
 }, 1000);
