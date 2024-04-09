4b95e389decd88fc43da48cc8e49015b98dfc2fe
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-04-08 21:40:28 -0600

    Updated Spring TA info
    Updated dates and out of office message.

--- scripts/ta.js
+++ scripts/ta.js
@@ -1,4 +1,5 @@
 const classes = document.querySelector('.ta-classes')
+const help = document.querySelector('.ta-help')
 const calendly = document.querySelector('.ta-calendly')
 const calendlyWidget = document.querySelector('.calendly-inline-widget');
 const semester = document.querySelector('#semester');
@@ -9,19 +10,38 @@ const semester = document.querySelector('#semester');
 function displaySemester(data) {
     // let callink = data.calendly[0];
     const today = new Date();
+    // today = Date(2024, 3, 22);
     // console.log(new DATE(today));
     if (today >= new Date(2023, 11, 8) && today <= new Date(2024, 3, 9)) {
         semester.innerHTML = "WINTER 2024";
         // calendlyWidget.setAttribute('data-url', `${callink['winter']}`);
-    } else if (today >= new Date(2024, 3, 22) && today <= new Date(2024, 7, 14)) {
+    } else if (today >= new Date(2024, 3, 22) && today <= new Date(2024, 6, 14)) {
         semester.innerHTML = "SPRING 2024";
         // calendlyWidget.setAttribute('data-url', `${callink['spring']}`);
-    } else if (today >= new Date(2024, 9, 11) && today <= new Date(2024, 11, 14)) {
+    } else if (today >= new Date(2024, 8, 16) && today <= new Date(2024, 11, 17)) {
         semester.innerHTML = "FALL 2024";
         // calendlyWidget.setAttribute('data-url', `${callink['fall']}`);
     } else {
         classes.style.display = "none";
-        calendly.innerHTML = `<h2>I am currently out of office. Please check back when the next semester begins.</h2>`;
+        help.style.display = "none";
+
+        winter = new Date(2025, 0, 8);
+        spring = new Date(2024, 3, 22);
+        fall = new Date(2024, 8, 16);
+
+        timer = spring - today;
+        days = 1000 * 60 * 60 * 24;
+        if (Math.floor(timer / days) > 1) {
+            calendly.innerHTML = `<h2>I am currently out of office. Please check back when the next semester begins.</h2>
+            <h3>The next semester begins in <ins>${Math.floor(timer / days)}</ins> days on ${spring.toDateString()}</h3>
+            <p>If you need to contact me, please use Canvas messaging or Microsoft Teams.</p>
+            `;
+        } else {
+            calendly.innerHTML = `<h2>I am currently out of office. Please check back when the next semester begins.</h2>
+            <h3>The next semester begins in <ins>${Math.floor(timer / days)}</ins> day on ${spring.toDateString()}</h3>
+            <p>If you need to contact me, please use Canvas messaging or Microsoft Teams.</p>
+            `;
+        }
         // calendlyWidget.setAttribute('data-url', `${callink.out}`);
     }
 }
